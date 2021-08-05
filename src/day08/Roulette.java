b5c2aeabd200cacdf9888c7ad865916d529f8791
yn0315 <yn0315@naver.com>
2021-08-05 16:10:49 +0900

    08.05 숙제

--- src/day08/Roulette.java
+++ src/day08/Roulette.java
@@ -14,7 +14,7 @@ public class Roulette {
         int playerNum = sc.nextInt();
 
         //인원 수의 범위가 적당한지 확인
-        if (playerNum <2 || playerNum >4) {
+        if (playerNum < 2 || playerNum > 4) {
             System.out.println("인원수가 올바르지 않습니다. game over");
             return;
         }
@@ -26,11 +26,11 @@ public class Roulette {
         //참여인원 수대로 이름을 반복입력 받은 후 배열에 저장
         System.out.println("\n플레이어 이름을 등록합니다.");
         for (int i = 0; i < players.length; i++) {
-            System.out.printf("%d번 플레이어의 이름:\n", i+1);
+            System.out.printf("%d번 플레이어의 이름:\n", i + 1);
             players[i] = sc.next();
         }
-            //참여인원 이름 출력
-            System.out.println(Arrays.toString(players) + "참가!");
+        //참여인원 이름 출력
+        System.out.println(Arrays.toString(players) + "참가!");
 
         System.out.print("\n 실탄개수 (6 미만) ==>");
         int bulletNum = sc.nextInt();
@@ -56,7 +56,7 @@ public class Roulette {
         //================게임 시작 로직 =================================//
         //선턴 플레이어 랜덤 결정
         //랜덤인덱스 생성 (플레이어 수에 따라 변경)
-        int turn = (int)(Math.random() * playerNum);
+        int turn = (int) (Math.random() * playerNum);
 
         System.out.printf("\n총을 돌렸습니다. %s부터 시작합니다.\n", players[turn]);
 
@@ -64,29 +64,69 @@ public class Roulette {
         //1. 탄창을 돌린다: 0 ~5번 사이의 인덱스를 랜덤 결정한다.
         //2. 격발한다: 위에서 나온 인덱스를 통해 탄창을 초회하여 실탄이 있는지 확인
         //3. 사망판정: 실탄이 있었다면 사망처리, 없었다면 다음 턴으로 넘어감
-
+        int idx = -1;
         while (true) {
+
             System.out.printf("\n[%s님의 턴] 실린더를 돌립니다.\n", players[turn]);
-            int bulletPosition = (int)(Math.random() *magazine.length);
+            int bulletPosition = (int) (Math.random() * magazine.length);
             System.out.println("#엔터를 누르면 격발합니다.");
             sc.nextLine();
 
             //사망판정
             if (magazine[bulletPosition]) {
-                System.out.printf("\n빵!![%s]님 사망..\n" , players[turn]);
-                break;
+                System.out.printf("\n빵!![%s]님 사망..\n", players[turn]);
+                bulletNum--;
+
+                //idx번호 찾기//숙제
+                for (int i = 0; i < players.length; i++) {
+                    if (players[turn].equals(players[i]))
+                        idx = i;
+
+                }
+
+                //플레이어 삭제를 위해 임시배열생성
+                String[] temp = new String[players.length - 1];
+
+                //원본배열 위치 옮기기(삭제)
+                for (int i = idx; i < players.length - 1; i++) {
+                    players[i] = players[i + 1];
+                }
+                //배열 복사
+                for (int i = 0; i < temp.length; i++) {
+                    temp[i] = players[i];
+                }
+                //원본배열에 옮기기
+                players = temp;
+                temp = null;
+
+                System.out.println(Arrays.toString(players));
+                System.out.println(bulletNum);
+
+                //숙제//총알이 0이 아니면 턴을 넘겨라
+                if (bulletNum != 0) {
+                    turn++;
+                    if (turn == players.length - 1) {
+                        turn = 0;
+                    }//end in inner if
+                }else {
+                    sc.close();
+                    break;
+                }
+                //end in outer if
+
+
             } else {
                 //생존한 경우: 턴을 넘김
                 System.out.println("휴.. 살았습니다.");
-                if (turn == players.length-1) {
+                if (turn == players.length - 1) {
                     //마지막 플레이어는 턴을 첫번째로 돌려놓는다.
                     turn = 0;
                 } else {
                     turn++;
-                }
+                }//end inner if
 
-            }
-        }
+            }//end outer if
+        }//end while
 
 
     }//end main
