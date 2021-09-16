de36849ac6e6cd5240ca07a3cc2e58aa5c6d9285
yn0315 <yunu0315@naver.com>
2021-09-16 11:32:23 +0900

    09.16 day19main 클래스 추가

---
+++ src/day19/inter/Main.java
@@ -0,0 +1,7 @@
+package day19.inter;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person = new Person();
+    }
+}
--- src/day19/inter/Person.java
+++ src/day19/inter/Person.java
@@ -1,9 +1,15 @@
 package day19.inter;
 
+import java.util.Arrays;
+
 public class Person {
 
     //애완동물들
-    Pet[] myPets = {new Dog(), new Cat()};
+    static Pet[] myPets = {new Dog(), new Cat()};
+
+    public Person() {
+        playWithPets();
+    }
 
     //애완동물들과 노는 기능
     public void playWithPets() {
@@ -11,5 +17,6 @@ public class Person {
             myPet.play();
         }
     }
+
 
 }
